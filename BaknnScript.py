edc580f51865847730aa72116e2459814cecaf49
roopaliv <roopali_vij@live.com>
2017-07-11 19:48:04 -0400

    Add files via upload

---
+++ BaknnScript.py
@@ -0,0 +1,153 @@
+import numpy as np
+from scipy.optimize import minimize
+from scipy.io import loadmat
+from math import sqrt
+
+
+def initializeWeights(n_in, n_out):
+    """
+    # initializeWeights return the random weights for Neural Network given the
+    # number of node in the input layer and output layer
+
+    # Input:
+    # n_in: number of nodes of the input layer
+    # n_out: number of nodes of the output layer
+
+    # Output:
+    # W: matrix of random initial weights with size (n_out x (n_in + 1))"""
+
+    epsilon = sqrt(6) / sqrt(n_in + n_out + 1)
+    W = (np.random.rand(n_out, n_in + 1) * 2 * epsilon) - epsilon
+    return W
+
+
+def sigmoid(z):
+    """# Notice that z can be a scalar, a vector or a matrix
+    # return the sigmoid of input z"""
+    s = 1.0 / (1.0 + np.exp(-1.0 * z))
+    return s
+
+
+def preprocess():
+    """ Input:
+     Although this function doesn't have any input, you are required to load
+     the MNIST data set from file 'mnist_all.mat'.
+
+     Output:
+     train_data: matrix of training set. Each row of train_data contains 
+       feature vector of a image
+     train_label: vector of label corresponding to each image in the training
+       set
+     validation_data: matrix of training set. Each row of validation_data 
+       contains feature vector of a image
+     validation_label: vector of label corresponding to each image in the 
+       training set
+     test_data: matrix of training set. Each row of test_data contains 
+       feature vector of a image
+     test_label: vector of label corresponding to each image in the testing
+       set
+
+     Some suggestions for preprocessing step:
+     - feature selection"""
+
+    mat = loadmat('mnist_all.mat')  # loads the MAT object as a Dictionary
+
+    # Pick a reasonable size for validation data
+
+    # ------------Initialize preprocess arrays----------------------#
+    train_preprocess = np.zeros(shape=(50000, 784))
+    validation_preprocess = np.zeros(shape=(10000, 784))
+    test_preprocess = np.zeros(shape=(10000, 784))
+    train_label_preprocess = np.zeros(shape=(50000,))
+    validation_label_preprocess = np.zeros(shape=(10000,))
+    test_label_preprocess = np.zeros(shape=(10000,))
+    # ------------Initialize flag variables----------------------#
+    train_len = 0
+    validation_len = 0
+    test_len = 0
+    train_label_len = 0
+    validation_label_len = 0
+    # ------------Start to split the data set into 6 arrays-----------#
+    for key in mat:
+        # -----------when the set is training set--------------------#
+        if "train" in key:
+            label = key[-1]  # record the corresponding label
+            tup = mat.get(key)
+            sap = range(tup.shape[0])#comment by roopali - will give shape i.e. len(tup) @ 0, 784 @ 1
+            tup_perm = np.random.permutation(sap)
+            tup_len = len(tup)  # get the length of current training set
+            tag_len = tup_len - 1000 # defines the number of examples which will be added into the training set
+
+            # ---------------------adding data to training set-------------------------#
+            train_preprocess[train_len:train_len + tag_len] = tup[tup_perm[1000:], :]
+            train_len += tag_len
+
+            train_label_preprocess[train_label_len:train_label_len + tag_len] = label
+            train_label_len += tag_len
+
+            # ---------------------adding data to validation set-------------------------#
+            validation_preprocess[validation_len:validation_len + 1000] = tup[tup_perm[0:1000], :]
+            validation_len += 1000
+
+            validation_label_preprocess[validation_label_len:validation_label_len + 1000] = label
+            validation_label_len += 1000
+
+            # ---------------------adding data to test set-------------------------#
+        elif "test" in key:
+            label = key[-1]
+            tup = mat.get(key)
+            sap = range(tup.shape[0])#comment by roopali - will give shape i.e. len(tup) @ 0, 784 @ 1
+            tup_perm = np.random.permutation(sap)
+            tup_len = len(tup)
+            test_label_preprocess[test_len:test_len + tup_len] = label
+            test_preprocess[test_len:test_len + tup_len] = tup[tup_perm]
+            test_len += tup_len
+            # ---------------------Shuffle,double and normalize-------------------------#
+    train_size = range(train_preprocess.shape[0])
+    train_perm = np.random.permutation(train_size)
+    train_data = train_preprocess[train_perm]
+    train_data = np.double(train_data)
+    train_data = train_data / 255.0
+    train_label = train_label_preprocess[train_perm]
+
+    validation_size = range(validation_preprocess.shape[0])
+    vali_perm = np.random.permutation(validation_size)
+    validation_data = validation_preprocess[vali_perm]
+    validation_data = np.double(validation_data)
+    validation_data = validation_data / 255.0
+    validation_label = validation_label_preprocess[vali_perm]
+
+    test_size = range(test_preprocess.shape[0])
+    test_perm = np.random.permutation(test_size)
+    test_data = test_preprocess[test_perm]
+    test_data = np.double(test_data)
+    test_data = test_data / 255.0
+    test_label = test_label_preprocess[test_perm]
+
+
+    # Feature selection
+    print('No of features before reduction')
+    print(train_data.shape[1])
+
+    feature_reduction=np.all(train_data == train_data[0,:] , axis=0)
+    inverted_feature_reduction = np.invert(feature_reduction)
+    
+    train_data=train_data[:,inverted_feature_reduction];
+    validation_data=validation_data[:,inverted_feature_reduction];
+    test_data=test_data[:,inverted_feature_reduction];
+    
+    print('No of features after reduction')
+    print((train_data.shape[1]))
+
+    # Your code here.
+
+    print('preprocess done')
+
+    return train_data, train_label, validation_data, validation_label, test_data, test_label
+
+
+def main():
+    print "hello"
+    preprocess();
+if __name__ == "__main__":
+  main()
